b5c25bd06eff86ca75103355dd050370c9e11827
normdoow <noahbragg@cedarville.edu>
2024-02-14 05:50:13 -0800

    hardhat setup and packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,38 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic:
+          process.env.TEST_MNEMONIC ??
+          "test test test test test test test test test test test junk",
+      },
+    },
+    base: {
+      url: process.env.BASE_GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    "goerli-optimism": {
+      url: process.env.OPTIMISM_GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY ?? "",
+    // apiKey: {
+    //   optimisticEthereum: process.env.ETHERSCAN_API_KEY ?? "",
+    //   optimisticGoerli: process.env.ETHERSCAN_API_KEY ?? "",
+    // },
+  },
 };
 
 export default config;
